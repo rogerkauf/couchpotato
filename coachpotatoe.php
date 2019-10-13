b5d940ce6f9cd8bc27bbde2e3341bf76a9017cae
rogerkauf <56504089+rogerkauf@users.noreply.github.com>
2019-10-13 05:10:26 -0500

    Create coachpotatoe.php

---
+++ coachpotatoe.php
@@ -0,0 +1,19 @@
+{
+    "results": [
+        {
+            "release_name": "Movie.Name.2008.1080p.AC-3.BluRay-ReleaseGroup",
+            "torrent_id": "123",
+            "details_url": "http://yourhost.com/torrents.php?id=123",
+            "download_url": "https://yourhost.com/download.php?ìd=123",
+            "imdb_id": "tt0123938",
+            "freeleech": true,
+            "type": "movie",
+            "size": 4088,
+            "leechers": 0,
+            "seeders": 0,
+            "publish_date": "2017-01-17T07:22:00Z"
+        },
+        ...
+    ],
+    "total_results": 4
+}
